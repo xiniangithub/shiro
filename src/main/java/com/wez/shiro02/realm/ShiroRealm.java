576321622389357df8b1aae2c2f047c384649ea9
xiaoer <xiaoer@git.com>
2017-11-07 08:23:04 +0800

    shiro - 基本搭建

---
+++ src/main/java/com/wez/shiro02/realm/ShiroRealm.java
@@ -0,0 +1,25 @@
+package com.wez.shiro02.realm;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.realm.Realm;
+
+public class ShiroRealm implements Realm {
+
+	public AuthenticationInfo getAuthenticationInfo(AuthenticationToken arg0) throws AuthenticationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean supports(AuthenticationToken arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
